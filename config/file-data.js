66901b56b53506a5a211ff8f722512f354b10b93
Bengsiswanto <bengsiswantoh@gmail.com>
2022-06-03 16:28:49 +0700

    update config

--- config/file-data.js
+++ config/file-data.js
@@ -1,1 +1,4 @@
-module.exports = { eldenRing: "data/elden-ring.json" };
+module.exports = {
+  eldenRing: "data/elden-ring.json",
+  epicGames: "data/epic-games.json",
+};
--- config/website.js
+++ config/website.js
@@ -1,6 +1,11 @@
 module.exports = {
-  eldenRing:
-    "https://api.github.com/repos/LukeYui/EldenRingSeamlessCoopRelease/releases",
-  epicGame:
-    "https://store-site-backend-static-ipv4.ak.epicgames.com/freeGamesPromotions?locale=en-US&country=ID&allowCountries=ID",
+  eldenRing: {
+    source:
+      "https://api.github.com/repos/LukeYui/EldenRingSeamlessCoopRelease/releases",
+  },
+  epicGames: {
+    source:
+      "https://store-site-backend-static-ipv4.ak.epicgames.com/freeGamesPromotions?locale=en-US&country=ID&allowCountries=ID",
+    generate: "https://store.epicgames.com/en-US/p/",
+  },
 };
