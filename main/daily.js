6a8b62c6c898e9fd70c8878a8d054f0a8ab948bc
Bengsiswanto <bengsiswantoh@gmail.com>
2022-06-03 17:15:19 +0700

    update daily

--- main/daily.js
+++ main/daily.js
@@ -1,7 +1,26 @@
+require("dotenv").config();
+
+const { sendContents } = require("../helper/discord");
+
 const eldenRing = require("../lib/elden-ring");
+const epicGames = require("../lib/epic-games");
 
 const main = async () => {
-  await eldenRing();
+  const urls = process.env.DISCORD_ERROR.split(",");
+
+  try {
+    await eldenRing();
+  } catch (err) {
+    console.log(err);
+    sendContents(err.message, urls);
+  }
+
+  try {
+    await epicGames();
+  } catch (err) {
+    console.log(err);
+    sendContents(err.message, urls);
+  }
 };
 
 main();
