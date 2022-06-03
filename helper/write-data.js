2ecf14ce5a4169677bfc6905ff9c88440c8ab584
Bengsiswanto <bengsiswantoh@gmail.com>
2022-06-03 16:30:49 +0700

    move some lib to helper

--- helper/write-data.js
+++ helper/write-data.js
@@ -2,8 +2,7 @@ require("dotenv").config();
 const fs = require("fs");
 
 const main = (filename, data) => {
-  data[name] = newData;
-  fs.writeFileSync(process.env.FILENAME, JSON.stringify(data));
+  fs.writeFileSync(filename, JSON.stringify(data));
 };
 
 module.exports = main;
