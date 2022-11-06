e2ffdbba835eb37d14f920a40e81bbf23df1d9e8
bengsiswanto <bengsiswantoh@gmail.com>
2022-11-06 22:27:03 +0700

    remove axios

--- helper/discord.js
+++ helper/discord.js
@@ -1,25 +1,26 @@
-require("dotenv").config();
-const axios = require("axios");
+require('dotenv').config();
 
 const sendContents = async (content, urls) => {
   if (!content || !urls) {
     return;
   }
 
-  const env = process.env.NODE_ENV || "development";
+  const env = process.env.NODE_ENV || 'development';
 
-  if (env === "development") {
-    console.log("urls", urls);
-    console.log("content");
+  if (env === 'development') {
+    console.log('urls', urls);
+    console.log('content');
     console.log(content);
   } else {
     for (const url of urls) {
       try {
-        await axios({
-          url,
-          method: "post",
-          data: { content },
-        });
+        const fetchOptions = {
+          method: 'POST',
+          headers: { 'Content-Type': 'application/json' },
+          body: JSON.stringify({ content }),
+        };
+        const response = await fetch(url, fetchOptions);
+        // console.log(await response.json());
       } catch (err) {
         console.log(err);
       }
@@ -28,7 +29,7 @@ const sendContents = async (content, urls) => {
 };
 
 const sendAlert = async (error) => {
-  const urls = process.env.DISCORD_ERROR.split(",");
+  const urls = process.env.DISCORD_ERROR.split(',');
 
   console.log(error);
   await sendContents(error.stack.toString(), urls);
