1b3ec88256f7b8140e359e214e0c9ed3d7cc1f23
Bengsiswanto <bengsiswantoh@gmail.com>
2023-10-13 11:29:46 +0700

    add godotfbx to main

--- main/daily.js
+++ main/daily.js
@@ -1,16 +1,16 @@
-const bareksa = require('../lib/bareksa');
 const eldenRing = require('../lib/elden-ring');
 const epicGames = require('../lib/epic-games');
+const godotFBX = require('../lib/godot-fbx');
 const humbleBundle = require('../lib/humble-bundle');
 const makmur = require('../lib/makmur');
 const openhab = require('../lib/openhab');
 
 const main = async () => {
-  // bareksa();
   eldenRing();
   epicGames();
   humbleBundle();
-  makmur();
+  godotFBX();
+  // makmur();
   openhab();
 };
 
--- main/test.js
+++ main/test.js
@@ -2,6 +2,7 @@ const bareksa = require('../lib/bareksa');
 const eldenRing = require('../lib/elden-ring');
 const epicGames = require('../lib/epic-games');
 const fanatical = require('../lib/fanatical');
+const godotFBX = require('../lib/godot-fbx');
 const humbleBundle = require('../lib/humble-bundle');
 const makmur = require('../lib/makmur');
 const openhab = require('../lib/openhab');
@@ -12,10 +13,11 @@ const main = async () => {
   // eldenRing();
   // epicGames();
   // fanatical();
+  // godotFBX();
   // humbleBundle();
-  // makmur();
+  makmur();
   // openhab();
-  tokopedia();
+  // tokopedia();
 };
 
 main();
